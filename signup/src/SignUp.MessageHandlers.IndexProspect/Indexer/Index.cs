9434baec77b5ab2d90abd012e54eeefacef4978e
Elton Stoneman <elton@sixeyed.com>
2017-09-06 19:39:51 +0100

    Add metrics code to handlers

--- signup/src/SignUp.MessageHandlers.IndexProspect/Indexer/Index.cs
+++ signup/src/SignUp.MessageHandlers.IndexProspect/Indexer/Index.cs
@@ -12,20 +12,13 @@ namespace SignUp.MessageHandlers.IndexProspect.Indexer
             var settings = new ConnectionSettings(node);
             var client = new ElasticClient(settings);
             client.CreateIndex("prospects");
-        }        
+        }
 
         public static void CreateDocument(Prospect prospect)
         {
-            try
-            {
-                var node = new Uri(Config.ElasticsearchUrl);
-                var client = new ElasticClient(node);                
-                client.Index(prospect, idx => idx.Index("prospects"));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Index prospect FAILED, email address: {prospect.EmailAddress}, ex: {ex}");
-            }
+            var node = new Uri(Config.ElasticsearchUrl);
+            var client = new ElasticClient(node);
+            client.Index(prospect, idx => idx.Index("prospects"));
         }
     }
 }
