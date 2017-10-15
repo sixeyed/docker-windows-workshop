f064b90ac6b072ac5eedbfee76728410ce1e19d0
Elton Stoneman <elton@sixeyed.com>
2017-10-15 15:52:56 +0200

    Fix host script

--- part-6/write-hosts.ps1
+++ part-6/write-hosts.ps1
@@ -1,9 +1,14 @@
 
-$ip = docker container inspect --format '{{ .NetworkSettings.Networks.nat.IPAddress }}' jenkins
-Add-Content -Path 'C:\Windows\System32\drivers\etc\hosts' "$ip jenkins.local"
+$ipFormat ='{{ .NetworkSettings.Networks.nat.IPAddress }}'
+$hosts = 'C:\Windows\System32\drivers\etc\hosts'
 
-$ip = docker container inspect --format '{{ .NetworkSettings.Networks.nat.IPAddress }}' bonobo
-Add-Content -Path 'C:\Windows\System32\drivers\etc\hosts' "$ip bonobo.local"
+$ip = docker container inspect --format $ipFormat infrastructure_jenkins_1
+Add-Content -Path $hosts "$ip jenkins.local"
 
-$ip = docker container inspect --format '{{ .NetworkSettings.Networks.nat.IPAddress }}' registry
-Add-Content -Path 'C:\Windows\System32\drivers\etc\hosts' "$ip registry.local"
+$ip = docker container inspect --format $ipFormat infrastructure_bonobo_1
+Add-Content -Path $hosts "$ip bonobo.local"
+
+$ip = docker container inspect --format $ipFormat infrastructure_registry_1
+Add-Content -Path $hosts "$ip registry.local"
+
+Get-Content -Path $hosts
