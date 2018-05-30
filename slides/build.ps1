3ffc195b4e3c42d273e282c23d7d66ead847e21a
Elton Stoneman <elton@sixeyed.com>
2018-05-30 03:28:20 +0200

    Add intro slides

--- slides/build.ps1
+++ slides/build.ps1
@@ -1,11 +1,13 @@
 
 # TODO - parameterize GH base link
-$baseUrl = 'https://raw.githubusercontent.com/sixeyed/docker-windows-workshop/master/'
+$rawUrl = 'https://raw.githubusercontent.com/sixeyed/docker-windows-workshop/devsum18/'
+$repoUrl = 'https://github.com/sixeyed/docker-windows-workshop/blob/devsum18/'
 
 $content = Get-Content ..\part-*.md -Raw
 #$content = Get-Content ..\part-1.md -Raw
 
-$content = $content.Replace('](/', "]($baseUrl")
+$content = $content.Replace('](/', "]($rawUrl")
+$content = $content.Replace('](./', "]($repoUrl")
 #$content = $content.Replace('{_x}', '<i class="fas fa-keyboard"></i>')
 #$content = $content.Replace('{_i}', '<i class="fas fa-info-circle"></i>')
 $(Get-Content template.html).Replace('${content}', $content) | Out-File .\index.html
