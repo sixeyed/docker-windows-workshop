85a105e005777e827ba616159f977ab9ec90fc5f
Elton Stoneman <elton@sixeyed.com>
2018-08-30 07:40:42 -0600

    Fix build for Netlify

--- slides/netlify/build.sh
+++ slides/netlify/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Building branch: $BRANCH"
 
-rawUrl="https://raw.githubusercontent.com/sixeyed/docker-windows-workshop/$BRANCH/"
 repoUrl="https://github.com/sixeyed/docker-windows-workshop/blob/$BRANCH/"
 
 markdownList=$(<"./contents/$BRANCH.txt")
@@ -9,9 +8,10 @@ html=""
 for markdownFile in $markdownList
 do
     echo "Adding content from: $markdownFile"    
-    sed -i -e "s#](/#]($rawUrl#g" $markdownFile
-    section="<section data-markdown="\""sections/$markdownFile"\""></section>\n"
-    html=$html$section    
+    sed -i '' -e "s#](./#]($repoUrl#g" "./sections/$markdownFile"
+    section="<section data-markdown="\""sections/$markdownFile"\""></section>"
+    html=$html$section
 done
 
-#$(Get-Content template.html).Replace('${content}', $html) | Out-File .\index.html -Encoding UTF8
+placeholder='${content}'
+sed -e "s#$placeholder#$html#g" template.html > index.html
