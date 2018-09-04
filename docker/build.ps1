e5a25d6b101cdd2e09a3221c67fdc91af41ff27d
Elton Stoneman <elton@sixeyed.com>
2018-09-04 21:26:01 +0100

    Fix copy paths

--- docker/build.ps1
+++ docker/build.ps1
@@ -1,10 +1,10 @@
-docker image build -t dwwx/signup-web -f .\frontend-web\web\Dockerfile .
+docker image build -t dwwx/signup-web -f .\docker\frontend-web\v1\Dockerfile .
 
-docker image build -t dwwx/reference-data-api -f .\backend-rest-api\reference-data-api\Dockerfile .
+docker image build -t dwwx/reference-data-api -f .\docker\backend-rest-api\reference-data-api\Dockerfile .
 
-docker image build -t dwwx/save-handler -f .\backend-async-messaging\save-handler\Dockerfile .
+docker image build -t dwwx/save-handler -f .\docker\backend-async-messaging\save-handler\Dockerfile .
 
-docker image build -t dwwx/homepage -f .\frontend-reverse-proxy\homepage\Dockerfile .
+docker image build -t dwwx/homepage -f .\docker\frontend-reverse-proxy\homepage\Dockerfile .
 
-docker image build -t dwwx/reverse-proxy -f .\frontend-reverse-proxy\reverse-proxy\Dockerfile .
+docker image build -t dwwx/reverse-proxy -f .\docker\frontend-reverse-proxy\reverse-proxy\Dockerfile .
 
